ee17be95c014349f4d124335a4933ef6ead6b228
Bekjon Ibragimov <bekjon_98@mail.ru>
2023-03-05 13:41:28 +0200

    reveal text animation added

--- src/components/login/LoginContainer.jsx
+++ src/components/login/LoginContainer.jsx
@@ -1,8 +1,10 @@
 import "./Login.css";
 import GirlImage from '../../assets/girlFront.jpg';
 import LoginForm from "./LoginForm";
+import { useEffect, useRef } from "react";
 
 const LoginContainer = () => {
+
     return (
         <div className="login__container">
             <div className="login__left">
@@ -10,9 +12,36 @@ const LoginContainer = () => {
             </div>
             <div className="login__right">
                 <LoginForm />
+                <RevealText />
             </div>
         </div>
      );
+}
+
+
+const RevealText = () => {
+
+    const revealTextRef = useRef(null);
+
+    useEffect(() => {
+        const revealTextElement = revealTextRef.current;
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    revealTextElement.classList.add('text-focus-in');
+                } else revealTextElement.classList.remove('text-focus-in')
+            })
+        }, { threshold: 1, rootMargin: '10px' });
+
+        observer.observe(revealTextElement);
+
+        return () => observer.unobserve(revealTextElement);
+
+    },[])
+
+    return <div className="login__right_reveal">
+        <h1 className="login__right_reveal-text" ref={revealTextRef}>Destiny Arrives All The Same</h1>
+    </div>
 }
 
 export default LoginContainer;
--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,16 +1,24 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ThemeContext } from '../contexts/ThemeContext';
 import './animations.css';
 import "./Login.css";
 
 const LoginForm = () => {
 
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useContext(ThemeContext);
+
     const onSubmit = (e) => {
         e.preventDefault();
+        setIsLoggedIn(true);
+        navigate('/mypage');
     }
 
     return (
         <div className="form__login">
             <form className="form__wrapper" onSubmit={onSubmit}>
-                <h1 className="form__title">L<span id='offset'>o</span>gin</h1>
+                <h1 className="form__title">Login</h1>
                 <input className="form__email" placeholder="email" />
                 <input className="form__password" type={'password'} placeholder="password"  />
                 <button className="form__btn" type={'submit'}>Login</button>
