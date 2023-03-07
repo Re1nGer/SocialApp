587973571e2932ed6da2833ac5981fdd85b29540
Bekjon Ibragimov <bekjon_98@mail.ru>
2023-03-07 23:45:21 +0200

    profile photo upload added

--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -1,11 +1,25 @@
 import "./Profile.css";
 import ProfilePhoto from '../../assets/profileImage.jpg';
+import { useState } from "react";
 
 const ProfileInfo = () => {
+
+    const [profileImage, setProfileImage] = useState(null);
+
+    const handleProfileImageUpload = (event) => {
+        setProfileImage(URL.createObjectURL(event.target.files[0]));
+    }
+
     return (
         <div className="profile-info__container">
             <div className="profile-info__inner">
-                <img className="profile-info__image" src={ProfilePhoto} alt="profile" />
+                <div className="profile-info__image-container">
+                    <img className="profile-info__image" src={profileImage || ProfilePhoto} alt="profile" />
+                    <label htmlFor="file" id="file-id">
+                        <button type="button">Upload Profile Picture</button>
+                        <input aria-labelledby="file-id" className="profile-info__file-input" onChange={handleProfileImageUpload} id="file" accept="*" type='file' />
+                    </label>
+                </div>
                 <div className="profile-info__details">
                     <h1 className="profile-info__username">Username</h1>
                     <p className="profile-info__bio">
