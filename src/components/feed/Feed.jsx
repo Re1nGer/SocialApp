c1c6b71ee5f22ad97e27a167f15ae42dc6c2374f
Bekjon Ibragimov <bekjon_98@mail.ru>
2023-03-15 20:31:57 +0200

    Feed Components Refactored, Animated From Post Added

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,157 +1,88 @@
 import "./Feed.css";
-import ProfileImageCap from "../../assets/profileHeaderImage.jpg";
-import ProfileImage from '../../assets/profileImage.jpg';
-import { Icon } from '@iconify/react';
-import { Link } from "react-router-dom";
+import React from 'react';
+import { FeedMain } from "./FeedMain";
+import { FeedTrends } from "./FeedTrends";
+import { FeedProfile } from "./FeedProfile";
+import { FeedFollow } from "./FeedFollow";
+import { AnimatePresence, motion } from "framer-motion";
+import { Icon } from "@iconify/react";
 
 const Feed = () => {
-    return (
-        <div className="feed">
-            <div className="feed__inner">
-                <div className="feed__left">
-                    <FeedProfile />
-                    <br />
-                    <br />
-                    <FeedFollow />
-                </div>
-                <div className="feed__center">
-                    <FeedMain />
-                </div>
-                <div className="feed__right">
-                    <FeedTrends />
-                </div>
-            </div>
-        </div>
-     );
-}
 
-const FeedProfile = () => {
+    const [isPostFormOpen, setIsPostFormOpen] = React.useState(false);
+
+    const handleClose = () => {
+        setIsPostFormOpen(false);
+    }
+
+    const handleOpen = () => {
+        setIsPostFormOpen(true);
+    }
 
     return (
-        <div className="feed__profile">
-            <div className="feed__profile-cap">
-                <img className="feed__profile-cap_img" src={ProfileImageCap} alt={'cap'} />
-            </div>
-            <div className="feed__profile-info">
-                <div className="feed__profile-circle">
-                    <img className="feed__profile-circle_img" src={ProfileImage} alt="avatar" />
-                </div>
-                <div className="feed__profile-name">
-                    Megan
-                </div>
-                <div className="feed__profile-username">
-                    @megan_cli
-                </div>
-                <div className="feed__profile-stats">
-                    <div className="feed__profile-following">
-                        6684
-                        <br />
-                        Following
-                    </div>
-                    <div className="feed__profile-divider"></div>
-                    <div className="feed__profile-followers">
-                        9991
-                        <br />
-                        Followers
-                    </div>
-                </div>
-                <div className="feed__profile-link">
-                    <Link to={'/mypage'}>My Profile</Link>
-                </div>
-            </div>
-        </div>
-    )
-}
+        <>
+            <AnimatePresence>
+                { isPostFormOpen ? (
+                    <AnimatedPostForm
+                        key={'post-modal'}
+                        exit={{opacity: 0, scale: .5}}
+                        initial={{ opacity: 0, scale: .5 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        handleClose={handleClose}
+                    />
+                ) : null }
+            </AnimatePresence>
 
-
-const FeedFollow = () => {
-
-    return (
-        <div className="feed__follow">
-            <div className="feed__follow-title">
-                Who is to follow you
-            </div>
-
-            <div className="feed__follow-card">
-                <img className="feed__follow-card_img" src={ProfileImage} alt={'profile'} />
-                <div className="feed__follow-card_info">
-                    <div className="feed__follow-card_info-name">Product Hunt</div>
-                    <div className="feed__follow-card_info-username">@ProductHunt</div>
-                </div>
-                <button className="feed__follow-card_btn">Follow</button>
-            </div>
-
-            <div className="feed__follow-card">
-                <img className="feed__follow-card_img" src={ProfileImage} alt={'profile'} />
-                <div className="feed__follow-card_info">
-                    <div className="feed__follow-card_info-name">Product Hunt</div>
-                    <div className="feed__follow-card_info-username">@ProductHunt</div>
-                </div>
-                <button className="feed__follow-card_btn">Follow</button>
-            </div>
-
-            <div className="feed__follow-card">
-                <img className="feed__follow-card_img" src={ProfileImage} alt={'profile'} />
-                <div className="feed__follow-card_info">
-                    <div className="feed__follow-card_info-name">Product Hunt</div>
-                    <div className="feed__follow-card_info-username">@ProductHunt</div>
-                </div>
-                <button className="feed__follow-card_btn">Follow</button>
-            </div>
-
-            <div className="feed__follow-more">
-                Show More
-            </div>
-
-        </div>
-    )
-}
-
-const FeedMain = () => {
-
-
-    return (
-        <div className="feed__main">
-            <div className="feed__input-container">
-                <img className="feed__input-img" src={ProfileImage} alt="profile" />
-                <div className="feed__input-form_container">
-                    <input className="feed__input-input" placeholder="What's happening" />
-                    <div className="feed__input-options">
-                        <div className="feed__input--photo">
-                            <button className="feed__input-btn">
-                                <Icon icon="gg:image" fontSize={20} />
-                                Photo
-                            </button>
+                <div className="feed">
+                    <div className="feed__inner">
+                        <div className="feed__left">
+                            <FeedProfile />
+                            <br />
+                            <br />
+                            <FeedFollow />
                         </div>
-                        <div className="feed__input--video">
-                            <button className="feed__input-btn">
-                                <Icon icon="ic:twotone-slow-motion-video" fontSize={20} /> 
-                                Video
-                            </button>
+                        <div className="feed__center">
+                            <FeedMain onClick={handleOpen} />
                         </div>
-                        <div className="feed__input--thread">
-                            <button className="feed__input-btn">
-                                <Icon icon="ic:twotone-slow-motion-video" fontSize={20} />
-                                Thread
-                            </button>
-                        </div>
-                        <div className="feed__input--schedule">
-                            <button className="feed__input-btn">
-                                <Icon icon="fa6-solid:calendar-days" fontSize={20} />
-                                Schedule
-                            </button>
+                        <div className="feed__right">
+                            <FeedTrends />
                         </div>
                     </div>
                 </div>
-            </div>
-            <FeedPost />
-        </div>
-    )
+        </>
+        );
+    }
 
-}
+export const FeedPostFormModal = React.forwardRef(({ handleClose }, ref) => {
+
+    return (
+        <>
+            <div className="feed__post-form_overlay"></div>
+            <section className="feed__post-form" ref={ref}>
+                <div className="feed__post-form_title-container">
+                    <div className="feed__post-form_title">Create Post</div>
+                    <Icon className="feed__post-form_title-icon" icon="material-symbols:close" onClick={handleClose} />
+                </div>
+                <div className="feed__post-form_input-container">
+                    <textarea className="feed__post-form_input" placeholder="what's on your mind ?" />
+                </div>
+                <label for="feed__post-image" className="feed__post-image">Add an Image:</label>
+                <div className="feed__post-drag-drop">
+                    <input type="file" className="feed__post-image_input" id="feed__post-image" name="post-image" accept="image/*" multiple />
+                    <p>Drag and drop your files here</p>
+                    <div class="overlay"></div>
+                </div>
+                <button className="feed__post-form_upload-btn">Upload from computer</button>
+                <button className="feed__post-form_btn">Submit Post</button>
+            </section>
+        </>
+    );
+});
+
+const AnimatedPostForm = motion(FeedPostFormModal);
 
 
-const FeedPost = () => {
+export const FeedPost = () => {
 
     return (
         <div>
@@ -159,60 +90,5 @@ const FeedPost = () => {
         </div>
     )
 }
-
-const FeedTrends = () => {
-
-
-    return (
-        <div className="feed__trends">
-            <div className="feed__trends-title">
-                Trends For you
-            </div>
-            <div className="feed__trends-subtitle">
-                Trending in Sweden
-            </div>
-            <div className="feed__trends-hash_block">
-                <div className="feed__trends-title_container">
-                    <div className="feed__trends-hash_block-title">
-                        #SWE
-                    </div>
-                    <div className="feed__trends-hash_block-subtitle">
-                        97.7 k Tweets
-                    </div>
-                </div>
-                <div className="feed__trends-hash_block-icon">
-                    <Icon icon="ph:dots-three-outline" fontSize={20} />
-                </div>
-            </div>
-            <div className="feed__trends-hash_block">
-                <div className="feed__trends-title_container">
-                    <div className="feed__trends-hash_block-title">
-                        #SWE
-                    </div>
-                    <div className="feed__trends-hash_block-subtitle">
-                        97.7 k Tweets
-                    </div>
-                </div>
-                <div className="feed__trends-hash_block-icon">
-                    <Icon icon="ph:dots-three-outline" fontSize={20} />
-                </div>
-            </div>
-            <div className="feed__trends-hash_block">
-                <div className="feed__trends-title_container">
-                    <div className="feed__trends-hash_block-title">
-                        #SWE
-                    </div>
-                    <div className="feed__trends-hash_block-subtitle">
-                        97.7 k Tweets
-                    </div>
-                </div>
-                <div className="feed__trends-hash_block-icon">
-                    <Icon icon="ph:dots-three-outline" fontSize={20} />
-                </div>
-            </div>
-        </div>
-    )
-}
-
 
 export default Feed;
---
+++ src/components/feed/FeedFollow.jsx
@@ -0,0 +1,44 @@
+import ProfileImage from '../../assets/profileImage.jpg';
+
+export const FeedFollow = () => {
+
+    return (
+        <div className="feed__follow">
+            <div className="feed__follow-title">
+                Who is to follow you
+            </div>
+
+            <div className="feed__follow-card">
+                <img className="feed__follow-card_img" src={ProfileImage} alt={'profile'} />
+                <div className="feed__follow-card_info">
+                    <div className="feed__follow-card_info-name">Product Hunt</div>
+                    <div className="feed__follow-card_info-username">@ProductHunt</div>
+                </div>
+                <button className="feed__follow-card_btn">Follow</button>
+            </div>
+
+            <div className="feed__follow-card">
+                <img className="feed__follow-card_img" src={ProfileImage} alt={'profile'} />
+                <div className="feed__follow-card_info">
+                    <div className="feed__follow-card_info-name">Product Hunt</div>
+                    <div className="feed__follow-card_info-username">@ProductHunt</div>
+                </div>
+                <button className="feed__follow-card_btn">Follow</button>
+            </div>
+
+            <div className="feed__follow-card">
+                <img className="feed__follow-card_img" src={ProfileImage} alt={'profile'} />
+                <div className="feed__follow-card_info">
+                    <div className="feed__follow-card_info-name">Product Hunt</div>
+                    <div className="feed__follow-card_info-username">@ProductHunt</div>
+                </div>
+                <button className="feed__follow-card_btn">Follow</button>
+            </div>
+
+            <div className="feed__follow-more">
+                Show More
+            </div>
+
+        </div>
+    );
+};
