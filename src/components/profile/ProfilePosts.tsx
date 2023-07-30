747b49378d222ffae5e54f059fe4512b90eed2b4
Bekjon Ibragimov <bekjon_98@mail.ru>
2023-07-30 15:30:36 +0300

    profile posts loaded immediately

--- src/components/profile/ProfilePosts.tsx
+++ src/components/profile/ProfilePosts.tsx
@@ -1,18 +1,50 @@
 import Post from './PostCard'
 import { AnimatedPostInView } from './AnimatedPostInView'
 import IPost from '../../types/IPost'
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { axios } from "../../axios";
+import profileInfo from "../pages/ProfileInfo";
+import CircleLoader from "../loader/CircleLoader";
 
 export const ProfilePosts = (): JSX.Element => {
 
-  const { profileInfo: { userPosts } } = useContext(ThemeContext)
+  const { profileInfo: { userPosts, id }, accessToken } = useContext(ThemeContext)
 
-  if (userPosts.length === 0) return <h2 className={'text-white font-bold'}>No Posts</h2>
+  const [posts, setPosts] = useState<IPost[]>([])
+
+  const [isError, setIsError] = useState<boolean>(false)
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const fetchPostData = async (): Promise<void> => {
+    try {
+      setIsLoading(true)
+      const { data } = await axios.get<IPost[]>(`/api/v1/post/${id}/list`);
+      setPosts(data)
+    } catch (error) {
+      console.log(error)
+      setIsError(true)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (accessToken && id) fetchPostData();
+  }, [accessToken, id]);
+
+  if (isLoading) {
+    return <CircleLoader />
+  }
+
+
+  if (posts.length === 0) return <h2 className={'text-white font-bold'}>No Posts</h2>
+
 
   return (
     <>
-      {userPosts.map((post: IPost) => (
+      {posts.map((post: IPost) => (
         <AnimatedPostInView key={post.id}>
           <Post {...post} />
         </AnimatedPostInView>
